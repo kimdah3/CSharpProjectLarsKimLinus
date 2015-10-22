cc16d5ae3ae876606ec37370eb3b2c1bad68318a
LinusIvarsson <ivarsson.linus@gmail.com>
2015-10-22 15:35:04 +0200

    Börjat i DataSerializer.

--- Data/DataSerializer.cs
+++ Data/DataSerializer.cs
@@ -8,6 +8,22 @@ namespace Data
 {
     public class DataSerializer
     {
+       /* string filepath;
+      
         
-    }
+        public DataSerializer()
+        {
+            var folder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            filepath = System.IO.Path.Combine(folder, "Feeds");
+        }   
+
+        public void sparaTillFil()
+        {
+       
+            Feed feed = new Feed();
+            var xml = new System.Xml.Serialization.XmlSerializer(typeof(List<Feed>));
+        }
+
+
+    */}
 }
